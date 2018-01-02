84bbbc23b5ff823f726252172d9ffcbecde80459
jiangjingbo <jiangjingbo@qiyi.com>
2018-01-02 15:12:48 +0800

    更新版本号

---
+++ demo2/src/ctrip/android/demo2/TestForPro.java
@@ -0,0 +1,20 @@
+package ctrip.android.demo2;
+
+/**
+ * Created by jiangjingbo on 2017/6/18.
+ */
+
+public class TestForPro {
+
+    public static int video_int = 1;
+    private int open_aar = 2;
+    protected boolean isForTest  = true;
+    public  int getIntMethod(){
+        return open_aar <<1;
+    }
+
+    public void getOpenStatus(){
+        System.out.println("ddd");
+    }
+
+}
---
+++ sample/src/ctrip/android/sample/TestForMain.java
@@ -0,0 +1,20 @@
+package ctrip.android.sample;
+
+/**
+ * Created by jiangjingbo on 2017/6/18.
+ */
+
+public class TestForMain {
+
+    public static int video_int = 1;
+    private int open_aar = 2;
+    protected boolean isForTest  = true;
+    public static int getIntMethod(){
+        return 2;
+    }
+
+    public void getOpenStatus(){
+        System.out.println("ddd");
+    }
+
+}
